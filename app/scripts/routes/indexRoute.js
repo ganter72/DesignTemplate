4c166ef28448df6063c6dea373a2c4f20cf6a0b1
Gabriel Bravo <gbravo.alejandro@gmail.com>
2017-08-22 23:10:33 -0300

    Design changes

--- app/scripts/routes/indexRoute.js
+++ app/scripts/routes/indexRoute.js
@@ -1,22 +1,25 @@
-app.config(['$stateProvider', '$urlRouterProvider', 
-  function($stateProvider, $urlRouterProvider) {
+app.config(['$stateProvider', '$urlRouterProvider',
+  function ($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider
-    .state('main', {
-      url: '/',
-      views:{
-          'menu':{
-              templateUrl:'app/views/modules/menu.html'
-            },
-            'main':{
-              templateUrl:'app/views/modules/main.html',
-              controller:'indexController'
+      .state('main', {
+        url: '/',
+        views: {
+          'menu': {
+            templateUrl: 'app/views/modules/menu.html'
           },
-          'girls':{
-              templateUrl:'app/views/modules/girls.html'
+          'main': {
+            templateUrl: 'app/views/modules/main.html',
+            controller: 'indexController'
+          },
+          'girls': {
+            templateUrl: 'app/views/modules/girls.html'
+          },
+          'footer':{
+            templateUrl: 'app/views/modules/footer.html'
           }
-      },
-    });
+        },
+      });
     $urlRouterProvider.otherwise('/');
   }
 ]);
